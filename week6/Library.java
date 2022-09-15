7037dfaec069640da4bd0cada4c4d6e31e9710ff
Deimar <deimar.parn@voco.ee>
2022-09-15 08:39:39 +0100

    Week 6 - Ex101 "search functionality for library"

--- week6/Library.java
+++ week6/Library.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Library {
     private ArrayList<Book> books;
+
     public Library() {
         this.books = new ArrayList<Book>();
     }
@@ -11,9 +12,46 @@ public class Library {
     public void addBook(Book newBook) {
         this.books.add(newBook);
     }
-    public void printBooks(){
-        for (Book book: this.books) {
+
+    public void printBooks() {
+        for (Book book : this.books) {
             System.out.println(book);
         }
+    }
+
+    public ArrayList<Book> searchByTitle(String title) {
+        ArrayList<Book> found = new ArrayList<Book>();
+        for (Book book : this.books) {
+            if (book.title().equals(title)) {
+                found.add(book);
+            }
+        }
+        // iterate the list of books and add all the matching books to the list found
+
+        return found;
+    }
+
+    public ArrayList<Book> searchByPublisher(String publisher) {
+        ArrayList<Book> found = new ArrayList<Book>();
+        for (Book book : this.books) {
+            if (book.publisher().equals(publisher)) {
+                found.add(book);
+            }
+        }
+        // iterate the list of books and add all the matching books to the list found
+
+        return found;
+    }
+
+    public ArrayList<Book> searchByYear(int year) {
+        ArrayList<Book> found = new ArrayList<Book>();
+        for (Book book : this.books) {
+            if (book.year() == (year)) {
+                found.add(book);
+            }
+        }
+        // iterate the list of books and add all the matching books to the list found
+
+        return found;
     }
 }
