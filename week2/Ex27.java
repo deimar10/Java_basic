7dafeb8f1f96aa6ceab20536b656888df69ae1db
Deimar <deimar.parn@voco.ee>
2022-09-16 10:12:31 +0100

    "Week-2 Ex27, From one to a hundred"

---
+++ week2/Ex27.java
@@ -0,0 +1,15 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Ex27 {
+    public static void main(String[] args) {
+        int number = 100;
+        int count = 0;
+
+        while ( count < number){
+            count++;
+            System.out.println(count);
+        }
+    }
+}
