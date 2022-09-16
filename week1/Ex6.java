b480e1198aac83fb24fd704a3526a560b2ea71b8
Deimar <deimar.parn@voco.ee>
2022-09-16 10:09:23 +0100

    "Week-1 Ex6, addition "

---
+++ week1/Ex6.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        int number = 5;
+        int Number = 25;
+        int sum = number + Number;
+        System.out.println(sum);
+    }
+}
