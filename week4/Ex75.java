42122d6bbac0481b50bf636d2c115148e39730d0
Deimar <deimar.parn@voco.ee>
2022-09-16 10:28:09 +0100

    "Week-4 Ex75, Decreasing counter"

---
+++ week4/Ex75.java
@@ -0,0 +1,25 @@
+package week4;
+
+public class Ex75 {
+
+
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(2);
+
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+    }
+
+}
+
