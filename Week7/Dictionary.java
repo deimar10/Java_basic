4dda51144dec2bb2068d69e2c2920cd94de2321d
Deimar <deimar.parn@voco.ee>
2023-01-06 11:09:08 +0000

    "Week 7, Implemented new method, which returns the amount of words in the dictionary."

--- Week7/Dictionary.java
+++ Week7/Dictionary.java
@@ -13,10 +13,17 @@ public class Dictionary {
     public void add(String word, String translation){
         translations.put(word, translation);
     }
+    public int amountOfWords(){
+        return translations.size();
+    }
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
         dictionary.add("apina", "monkey");
         dictionary.add("banaani", "banana");
+        System.out.println(dictionary.amountOfWords());
+
+        dictionary.add("cembalo", "harpsichord");
+        System.out.println(dictionary.amountOfWords());
 
         System.out.println(dictionary.translate("apina"));
         System.out.println(dictionary.translate("porkkana"));
